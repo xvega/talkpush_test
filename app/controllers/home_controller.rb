a815abacebb1944bc1ae79901895b64c62867425
xavi <xabi1309@gmail.com>
2019-10-11 17:49:07 -0600

    Update home controller and views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
 class HomeController < ApplicationController
-  def index
+  def index; end
+
+  def candidates
+    #FIXME these 2 lines (and spreadsheet method) belong to the cron job task, not here.
     spreadsheet.reload_ws
     spreadsheet.request_new_candidates_creation
 
     @candidates = Candidate.all
+    @talk_push_candidates = RequestTalkPushCandidate.all
 
     respond_to do |format|
       format.js
